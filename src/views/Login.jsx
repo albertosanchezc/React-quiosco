55f428b7f9755b11ae92b4eda9893642e3f66e4d
albertosanchezc <albertosanchezc98@gmail.com>
2025-06-03 23:23:44 -0600

    Login y Registro junto con enlaces

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,5 +1,53 @@
 export default function Login() {
   return (
-    <div>Login</div>
+        <>
+      <h1 className="text-4xl font-black">Iniciar Sesión</h1>
+      <p>Para crear un pedido debes iniciar sesión</p>
+
+      <div className="bg-white shadow-md rounded-md mt-10 px-5 py-10">
+        <form action="">
+          <div className="mb-4">
+            <label htmlFor="email"
+              className="text-slate-800"
+            >Email</label>
+
+            <input
+              id="email"
+              type="email"
+              className="mt-2 w-full p-3 bg-gray-100"
+              name="email"
+              placeholder="Tu Email"
+            />
+          </div>
+
+
+          <div className="mb-4">
+            <label htmlFor="password"
+              className="text-slate-800"
+            >Password</label>
+
+            <input
+              id="password"
+              type="password"
+              className="mt-2 w-full p-3 bg-gray-100"
+              name="password"
+              placeholder="Tu Password"
+            />
+          </div>
+
+          <input
+            type="submit"
+            value="Crear Cuenta"
+            className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer"
+          />
+        </form>
+      </div>
+
+      <nav className="mt-5">
+        <a href="/auth/registro">
+          ¿No tienes cuenta? Crea una
+        </a>
+      </nav>
+    </>
   )
 }
--- src/views/Registro.jsx
+++ src/views/Registro.jsx
@@ -1,6 +1,85 @@
+import { Link } from "react-router-dom";
 
 export default function Registro() {
   return (
-    <div>Registro</div>
+    <>
+      <h1 className="text-4xl font-black">Crea tu Cuenta</h1>
+      <p>Crea tu Cuenta llenando el formulario</p>
+
+      <div className="bg-white shadow-md rounded-md mt-10 px-5 py-10">
+        <form action="">
+          <div className="mb-4">
+            <label htmlFor="name"
+              className="text-slate-800"
+            >Nombre</label>
+
+            <input
+              id="name"
+              type="text"
+              className="mt-2 w-full p-3 bg-gray-100"
+              name="name"
+              placeholder="Tu nombre"
+            />
+          </div>
+
+
+          <div className="mb-4">
+            <label htmlFor="email"
+              className="text-slate-800"
+            >Email</label>
+
+            <input
+              id="email"
+              type="email"
+              className="mt-2 w-full p-3 bg-gray-100"
+              name="email"
+              placeholder="Tu Email"
+            />
+          </div>
+
+
+          <div className="mb-4">
+            <label htmlFor="password"
+              className="text-slate-800"
+            >Password</label>
+
+            <input
+              id="password"
+              type="password"
+              className="mt-2 w-full p-3 bg-gray-100"
+              name="password"
+              placeholder="Tu Password"
+            />
+          </div>
+
+          <div className="mb-4">
+            <label htmlFor="password_confirmation"
+              className="text-slate-800"
+            >Repetir Password</label>
+
+            <input
+              id="password"
+              type="password"
+              className="mt-2 w-full p-3 bg-gray-100"
+              name="password"
+              placeholder="Repetir Password"
+            />
+          </div>
+
+          <input
+            type="submit"
+            value="Crear Cuenta"
+            className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer"
+          />
+        </form>
+      </div>
+
+      <nav className="mt-5">
+        {/* Lo siguiente es la manera de hacer un enlace sin flashaso */}
+        <Link to="/auth/login">
+          ¿Ya tienes cuenta? Inicia Sesión
+        </Link>
+      </nav>
+    </>
   )
 }
