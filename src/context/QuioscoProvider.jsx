9ea91d0e57ff08d40426394350f45d7668bd4584
albertosanchezc <albertosanchezc98@gmail.com>
2025-06-15 20:37:10 -0600

    Actualizar un producto en el pedido

--- src/context/QuioscoProvider.jsx
+++ src/context/QuioscoProvider.jsx
@@ -2,7 +2,7 @@ import { createContext, useState } from 'react'
 import { categorias as categoriasDB } from '../data/categorias'
 
 const QuioscoContext = createContext()
-const QuioscoProvider = ({children}) => {
+const QuioscoProvider = ({ children }) => {
 
     const [categorias, setCategorias] = useState(categoriasDB);
     const [categoriaActual, setcategoriaActual] = useState(categorias[0]);
@@ -18,25 +18,25 @@ const QuioscoProvider = ({children}) => {
         const categoria = categorias.filter(categoria => categoria.id === id)[0]
         setcategoriaActual(categoria)
         console.log(categoriaActual)
-        
-    } 
+
+    }
 
     const handleSetProducto = producto => {
         setProducto(producto)
     }
 
-    const handleAgregarPedido = ({categoria_id, imagen, ...producto}) => {
-        setPedido([...pedido,producto])
-        
-        if(pedido.some(pedidoState => pedidoState.id === pedido.id)){
-            const productoEdicion = pedido.filter( pedidoState => pedidoState.id === producto.id)[0]
-            setCantidad(productoEdicion.cantidad)
-            setEdicion
+    const handleAgregarPedido = ({ categoria_id, imagen, ...producto }) => {
+
+        if (pedido.some(pedidoState => pedidoState.id === producto.id)) {
+            const pedidoActualizado = pedido.map(pedidoState => pedidoState.id === producto.id ? producto : pedidoState)
+            setPedido(pedidoActualizado)
+        } else {
+            setPedido([...pedido, producto])
         }
     }
 
     return (
-        <QuioscoContext.Provider 
+        <QuioscoContext.Provider
             value={{
                 categorias,
                 categoriaActual,
@@ -48,8 +48,8 @@ const QuioscoProvider = ({children}) => {
                 pedido,
                 handleAgregarPedido
             }}
-        
-        
+
+
         >{children}</QuioscoContext.Provider>
     )
 }
