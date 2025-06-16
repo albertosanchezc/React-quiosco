c6912a442f3d69ef3c4708dafba5d4d1a2cf36b0
albertosanchezc <albertosanchezc98@gmail.com>
2025-06-15 21:05:38 -0600

    Deshabilitar botón de confirmar pedido si el pedido esta vacio

--- src/components/Resumen.jsx
+++ src/components/Resumen.jsx
@@ -4,6 +4,7 @@ import { formatearDinero } from "../helpers";
 
 export default function Resumen() {
   const { pedido,total } = useQuiosco();
+  const comprobarPedido = () => pedido.length === 0;
 
   return (
     <aside className='w-72 h-screen overflow-y-scroll p-5'>
@@ -39,8 +40,9 @@ export default function Resumen() {
         <div className="mt-5">
           <input
             type="submit"
-            className="bg-indigo-600 hover:bg-indigo-800 px-5 py-2 rounded uppercase text-white font-bold text-center w-full cursor-pointer"
+            className={`${comprobarPedido() ? 'bg-indigo-100' : 'bg-indigo-600  hover:bg-indigo-800' } px-5 py-2 rounded uppercase text-white font-bold text-center w-full cursor-pointer`}
             value='Confirmar Pedido'
+            disabled={comprobarPedido()}
           />
         </div>
       </form>
