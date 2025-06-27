985cf7916d7c77f5a5f55b573ab3678deca0bd04
albertosanchezc <albertosanchezc98@gmail.com>
2025-06-26 23:50:01 -0600

    Iniciando el panel de administración y protegiendolo

---
+++ src/views/Ordenes.jsx
@@ -0,0 +1,11 @@
+
+export default function Ordenes() {
+    return (
+        <div>
+            <h1 className='text-4xl font-black'>Ordenes</h1>
+            <p className="text-2xl my-10">
+                Administra las ordenes desde aquí
+            </p>
+        </div>
+    )
+}
